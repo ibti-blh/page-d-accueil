46555ecd86dd5c40371f9c36dc12b1bd072d0287
ibti-blh <ibtissembelhdid@gmail.com>
2024-11-16 16:49:40 +0100

    produit stock

---
+++ db_conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "produit";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connexion échouée : " . mysqli_connect_error());
+}
+
+echo "Connexion réussie à la base de données.";
+?>
